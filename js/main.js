0b1fb379a274a3734f86cee511de03c6d352c858
cfx78 <104265674+cfx78@users.noreply.github.com>
2022-11-26 22:13:37 -0600

    Adding carousel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import User from './objcons.js';
 import Person from './classes.js';
+import newUser from './factfunc.js';
 //Import
 let user01 = new User('ivo04', 'ivo@yahoo.com', 'password');
 
@@ -34,3 +35,8 @@ let user04 = new Mod('cfx789', 'cfx@yahoo.com', 'password');
 
 user04.log();
 user04.message();
+
+//Factory Function
+
+let user05 = newUser('buckXP', 'bxp@yahoo.com', 'password');
+user05.cnslMsg();
