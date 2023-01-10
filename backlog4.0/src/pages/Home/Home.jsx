ab04b54e03f46be9bdb77a01e581bfdbee7fc8f7
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-01-10 15:00:31 -0600

    Finishing styling of homepage and Modal

--- backlog4.0/src/pages/Home/Home.jsx
+++ backlog4.0/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import LoginModal from '../../components/LoginModal'
 import './Home.css'
 
 function Home() {
     return (
         <div className="bg-dark home--container">
             <p className="fw-bold text-light">Backlog</p>
+            <LoginModal />
         </div>
     )
 }
