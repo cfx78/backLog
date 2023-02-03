7f5cc9a4d4850ad4cd1db6a3e3d7c1875bc3f80a
cfx78 <104265674+cfx78@users.noreply.github.com>
2023-02-02 19:23:29 -0600

    Fixed the issue with getting user data

--- backlog4.0/src/components/GamesList.jsx
+++ backlog4.0/src/components/GamesList.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from 'react'
-import { getDoc, doc } from 'firebase/firestore'
+import { getDoc, doc, onSnapshot } from 'firebase/firestore'
 
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { db } from '../../Firebase.config'
@@ -10,41 +10,42 @@ import { db } from '../../Firebase.config'
 function GamesList() {
     const auth = getAuth()
     const [user, setUser] = useState({})
-    const [getUserGames, setGetUserGames] = useState([])
-
-    onAuthStateChanged(auth, (currentUser) => {
-        setUser(currentUser)
-    })
-
+    const [userGames, setUserGames] = useState([])
     const docRef = doc(db, 'users', `${user.uid}`)
-    useEffect(() => {
-        const setupUI = async () => {
-            const docSnap = await getDoc(docRef)
-            console.log(docSnap.data().games)
-            setGetUserGames(docSnap.data().games)
-        }
-        setupUI()
-    }, [])
-    console.log(getUserGames)
 
-    const gamesLog = getUserGames.map((game) => {
-        return (
-            <tr>
-                <td>
-                    <input className="checkbox" type="checkbox" name="" id="" />
-                </td>
-                <td className="text-bg-dark">{game}</td>
-                <td>
-                    <button className="btn btn-outline-light">
-                        Delete Game
-                    </button>
-                </td>
-            </tr>
-        )
+    onAuthStateChanged(auth, setUser)
+    async function setupUI() {
+        const docSnap = await getDoc(docRef)
+        console.log(docSnap.data())
+        setUserGames(docSnap.data().games)
+    }
+    const unsub = onSnapshot(docRef, (doc) => {
+        const source = doc.metadata.hasPendingWrites ? 'Local' : 'Server'
+        console.log(source, ' data: ', doc.data())
     })
+    unsub()
+
+    function componentDidUpdate() {
+        setupUI()
+    }
+    useEffect(componentDidUpdate, [user])
+
+    // useEffect(() => {
+    //     const setupUI = async () => {
+    //         const docSnap = await getDoc(docRef)
+    //         console.log(docSnap.data().games)
+    //         setUserGames(docSnap.data().games)
+    //     }
+    //     setupUI()
+    // }, [])
+    // console.log(userGames)
+    const gamesLog = userGames.map(toGamesLog)
+    console.log(userGames)
     return (
         <div>
-            <h1 className="display-1">${user.email}'s backlog</h1>
+            <h1 className="display-1">
+                {user.email} <br /> Backlog
+            </h1>
             <br />
             <br />
             <div className="table-responsive">
@@ -60,6 +61,20 @@ function GamesList() {
                 </table>
             </div>
         </div>
+    )
+}
+
+function toGamesLog(game) {
+    return (
+        <tr>
+            <td>
+                <input className="checkbox" type="checkbox" name="" id="" />
+            </td>
+            <td className="text-bg-dark">{game}</td>
+            <td>
+                <button className="btn btn-outline-light">Delete Game</button>
+            </td>
+        </tr>
     )
 }
 
